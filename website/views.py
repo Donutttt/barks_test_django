99ce338aae025141e53c1d97cebb051db8541e44
Oliver Radini <orr2@live.com>
2016-12-19 18:48:47 +0000

    added some styling

--- website/views.py
+++ website/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse, JsonResponse
+from django.forms import ModelForm
+from website.models import Animal
 
 import json
 
 from website.models import Animal
 
 def index(request):
-
-
     return render(request, 'website/index.html', {})
 
 
-def animals_data(request):
 
-    animals = Animal.objects.filter().values('name', 'description', 'age')
+def animals_data(request):
+    animals = Animal.objects.filter().values('name', 'description', 'age', 'type')
 
     dataDict = {
         "animals": animals,
     }
 
-
     return JsonResponse(list(animals), safe=False)
 
-
+def animals(request):
+    return render(request, 'website/animals.html', {})
 
 
 
 def test(request):
-    
     return HttpResponse("test reached return")
