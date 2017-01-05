f5822fbf4e4208ce9902f96c5e891f26ab3ef04a
Oliver Radini <orr2@live.com>
2017-01-05 23:46:04 +0000

    added material design to animal contact form

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AnimalContactForm(forms.Form):
-    nameField = forms.CharField(label="Your Name", max_length=100)
-    emailField = forms.EmailField(label="Email")
-    textField = forms.CharField(label="Your query", widget=forms.Textarea)
+    nameField = forms.CharField(label="Your Name", max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Your name'}))
+    emailField = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    textField = forms.CharField(label="Your query", widget=forms.Textarea(attrs={'placeholder': 'Your query'}))
